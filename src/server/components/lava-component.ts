39c725c9c6dc6ff35d2b5644ad38eea98dc61534
Hoarsekat <hoarsekat@outlook.com>
2025-05-07 20:29:14 +1000

    refactor: lava component checks for player health

--- src/server/components/lava-component.ts
+++ src/server/components/lava-component.ts
@@ -8,8 +8,17 @@ interface Attributes {}
 })
 export class LavaComponent extends BaseComponent<Attributes, BasePart> implements OnStart {
 	onStart() {
+		// part.Parent?.FindFirstChildOfClass("Humanoid")?.TakeDamage(100)
 		if (this.instance) {
-			this.instance.Touched.Connect((part) => part.Parent?.FindFirstChildOfClass("Humanoid")?.TakeDamage(100));
+			this.instance.Touched.Connect((otherPart: BasePart) => {
+				const parent = otherPart.Parent;
+				if (!parent) return;
+
+				const humanoid = parent.FindFirstChild("Humanoid") as Humanoid | undefined;
+				if (humanoid && humanoid.Health > 0) {
+					humanoid.TakeDamage(humanoid.MaxHealth);
+				}
+			});
 		}
 	}
 }
